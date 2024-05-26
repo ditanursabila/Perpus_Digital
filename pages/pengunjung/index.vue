7931e4433e56779a877f0a4c5d967baab6d246f4
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2024-05-26 20:16:16 +0700

    Add files via upload

--- pages/pengunjung/index.vue
+++ pages/pengunjung/index.vue
@@ -57,11 +57,11 @@ onMounted(() => {
             <tr v-for="(visitor, i) in visitors" :key="i">
               <td>{{ i + 1 }}.</td>
               <td>{{ visitor.nama }}</td>
-              <td>{{ visitor.keanggotaan.nama }}</td>
+              <td>{{ visitor.anggota }}</td>
               <td>{{ visitor.tingkat }} {{ visitor.jurusan }}</td>
               <td>{{ visitor.tanggal }}, {{ visitor.time }}</td>
               <td v-if="visitor.keperluan != null">
-                {{ visitor.keperluan.nama }}
+                {{ visitor.perlu }}
               </td>
               <td v-else="visitor.keperluan == null" class="text-muted">Tidak ada</td>
             </tr>
